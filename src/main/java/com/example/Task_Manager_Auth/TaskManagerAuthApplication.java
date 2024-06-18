bc7e5725ebc127dfaab6deb41832a2222085978a
aaron <redskin.rox@gmail.com>
2024-06-18 10:59:39 -0400

    init

---
+++ src/main/java/com/example/Task_Manager_Auth/TaskManagerAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.Task_Manager_Auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerAuthApplication.class, args);
+	}
+
+}
